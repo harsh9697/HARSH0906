12be24130628ef286c2ac628c6960a0aa4d92664
harsh9697 <harshchauhan9697@gmail.com>
2016-10-21 09:35:30 +0530

    Add files via upload

---
+++ largest of two numbers by harsh.py
@@ -0,0 +1,7 @@
+n1=int(input('enter a number n1:'))
+n2=int(input('enter a number n2:'))
+if (n1>n2):
+    print(n1,"n1 is the largest number")
+else:
+    print(n2, "n2 is the largest number")
+print('press enter to exit')    
